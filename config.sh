f22c99e11869a352c85196c7c4864301c72cea13
aniecesedhai <awitice23@gmail.com>
2024-04-14 12:55:04 +0545

    create role from cli to avoid conflict

--- config.sh
+++ config.sh
@@ -10,10 +10,10 @@ export KUBECONFIG=kubeconfig.yaml
 plan () {
     if [ $ROLE == "ClusterRole" ]; then
         helm upgrade $USERNAME --install roles/ --dry-run \
-        --set clusterrole.name=clusterrole --set clusterrolebinding.name=clusterrole-binding --set clusterrole.serviceaccount.name=$USERNAME --set clusterrole.secret.name=$USERNAME
+        --set clusterrole.name=clusterrole-$USERNAME --set clusterrolebinding.name=clusterrole-binding-$USERNAME --set clusterrole.serviceaccount.name=$USERNAME --set clusterrole.secret.name=$USERNAME
     else
         helm upgrade $USERNAME --install roles/ --dry-run \
-        --set role.namespace=$NAMESPACE --set role.name=developer-role --set rolebinding.name=developer-role-binding--set role.serviceaccount.name=$USERNAME --set role.secret.name=$USERNAME
+        --set role.namespace=$NAMESPACE --set rolebinding.name=developer-role-binding-$USERNAME --set role.serviceaccount.name=$USERNAME --set role.secret.name=$USERNAME
     fi
 }
 
@@ -21,10 +21,17 @@ plan () {
 apply() {
     if [ $ROLE == "ClusterRole" ]; then
         helm upgrade $USERNAME --install roles/ \
-        --set clusterrole.name=clusterrole --set clusterrolebinding.name=clusterrole-binding --set clusterrole.serviceaccount.name=$USERNAME --set clusterrole.secret.name=$USERNAME
+        --set clusterrole.name=clusterrole-$USERNAME --set clusterrolebinding.name=clusterrole-binding-$USERNAME --set clusterrole.serviceaccount.name=$USERNAME --set clusterrole.secret.name=$USERNAME
     else
+        kubectl get roles -n $NAMESPACE
+        role=$(kubectl -n $NAMESPACE get roles -o=jsonpath='{.items[*].metadata.name}')
+        if [[ $role =~ "developer-role" ]]; then
+          echo "Developer role already exists. Creating rolebinding" &> /dev/null
+        else
+          kubectl create role developer-role --verb=* --resource=* --namespace=$NAMESPACE
+        fi
         helm upgrade $USERNAME --install roles/ \
-        --set role.namespace=$NAMESPACE --set role.name=developer-role --set rolebinding.name=developer-role-binding --set role.serviceaccount.name=$USERNAME --set role.secret.name=$USERNAME
+        --set role.namespace=$NAMESPACE --set rolebinding.name=developer-role-binding-$USERNAME --set role.serviceaccount.name=$USERNAME --set role.secret.name=$USERNAME
     fi
 }
 if [ $ACTION == "plan" ]; then
